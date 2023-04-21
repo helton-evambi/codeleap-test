34491fe4077fcd25d4f6b1b822881ee536ed5586
Helton Evambi <heltonevambi@gmail.com>
2023-04-21 21:37:21 +0100

    feat: configure route structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Sigup from './pages/signup/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+        <Route element={<App />}>
+          <Route path="/signup" element={<Sigup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>,
 )
